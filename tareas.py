523d4059bf4d70bfc47e1487f465149841686457
JOHN BRYAN RAU ROSALES <johnraurosales@gmail.com>
2025-11-05 03:50:10 -0500

    Agrega función para consultar tareas

--- tareas.py
+++ tareas.py
@@ -4,3 +4,12 @@ tareas = []
 def agregar_tarea(tarea):
     tareas.append(tarea)
     print(f"Tarea '{tarea}' agregada.")
+
+
+def mostrar_tareas():
+    if not tareas:
+        print("No hay tareas.")
+    else:
+        print("Listado de tareas:")
+        for i, tarea in enumerate(tareas, 1):
+            print(f"{i}. {tarea}")
